ff0e5c23b776abb89e2f5c5ed04bfb1c8064163c
zhangzheng-zz <15521414675@163.com>
2020-10-11 22:24:57 +0800

    doc(readme): 添加github地址

--- README.md
+++ README.md
@@ -2,4 +2,5 @@
 # 个人博客
 #### 学习 | 总结 | 笔记 | 交流
 #### 博客地址: https://zhangzheng-zz.github.io/blog/
+#### github 地址: https://github.com/zhangzheng-zz/blog/issues
 
--- deploy.sh
+++ deploy.sh
@@ -6,8 +6,8 @@ echo "执行命令：cd ./_book\n"
 cd ./_book
 
 # 初始化一个仓库，仅仅是做了一个初始化的操作，项目里的文件还没有被跟踪
-# echo "执行命令：git init\n"
-# git init
+echo "执行命令：git init\n"
+git init
 
 # 保存所有的修改
 echo "执行命令：git add -A"
@@ -18,8 +18,8 @@ echo "执行命令：commit -m 'deploy'"
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-echo "执行命令：git push -f https://github.com/yulilong/book.git master:gh-pages"
-git push -f https://github.com/yulilong/book.git master:gh-pages
+echo "执行命令：git push -f https://github.com/zhangzheng-zz/blog master:gh-pages"
+git push -f https://github.com/zhangzheng-zz/blog master:gh-pages
 
 # 返回到上一次的工作目录
 echo "回到刚才工作目录"
